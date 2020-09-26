aa6822bd540508c9839e27527bf5ae798d04d028
Harmanjit Singh <hsingh426@myseneca.ca>
2020-09-25 22:38:16 -0400

    Updated

--- lab1_base_hsingh426.bash
+++ lab1_base_hsingh426.bash
@@ -1,71 +1,69 @@
 #!/usr/bin/sh
 
 # Print hostname (a)
-echo HOSTNAME:
+echo "HOSTNAME:\n"
 hostname
 
 # Gather Disk/Partition, Usage, Mount Points (b)
-echo DISKS
+echo "DISKS:\n"
 df
 
 # List of network device name (c,d)
-echo "NETWORK INTERFACES:" 
+echo "NETWORK INTERFACES:\n" 
 ip a 
 
 # OS/Kernel Version (e)
-echo "KERNEL INFO:"
+echo "KERNEL INFO:\n"
 cat /etc/os-release
 uname -r 
 
 # Security mode {SELinux} (f)
-echo "SELINUX:"
+echo "SELINUX:\n"
 sestatus
 
 # Firewall Config (g)
-echo "FIREWALL:"
+echo "FIREWALL:\n"
 sudo iptables-save
 
 # Active Repositories (h)
-echo "ACTIVE REPOSITORIES:"
+echo "ACTIVE REPOSITORIES:\n"
 sudo dnf repolist all 
 
-echo "RPM Packages: " 
+echo "RPM Packages:\n"
 rpm -qa | wc -l
 # ADD NAME OF ALL PACKAGES TO WORD (j)
 
 # DNS Server (k)
-echo "DNS SERVERS:"
+echo "DNS SERVERS:\n"
 cat /etc/resolv.conf
 
 # List all active users (l)
-echo "USERS:"
+echo "USERS:\n"
 users
 
 # Date/Time of Installation (m)
-echo "DATE OF INSTALL:"
+echo "DATE OF INSTALL:\n"
 uname -v
 
 # Hardware info
-echo "HARDWARE INFO:"
-#HW
-lshw
 
 #CPU
-echo "CPU INFO:"
+echo "CPU INFO:\n"
 lscpu
 
 #MEM
-echo "MEMORY INFO:"
+echo "MEMORY INFO:\n"
 free -h
 
 #DEV
-echo "DEVICES:"
+echo "DEVICES:\n"
 mount
 # Running services (o)
-echo "RUNNING SERVICES:"
+echo "RUNNING SERVICES:\n"
 systemctl list-unit-files -t service --no-pager | grep enabled | cut -d' ' -f1
 
 # Non-running services [Installed] (p)
+echo "NON-RUNNING SERVICES:\n"
 systemctl list-unit-files -t service --no-pager | grep disabled | cut -d' ' -f1
 
 # EXTRA (q)
