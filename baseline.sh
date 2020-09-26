b95803a615938dd3730a19c671794d4327a32dd4
Harmanjit Singh <hsingh426@myseneca.ca>
2020-09-25 22:04:34 -0400

    Updated config with echos

--- baseline.sh
+++ baseline.sh
@@ -1,36 +1,71 @@
 #!/usr/bin/sh
 
 # Print hostname (a)
-hostnamectl --static
+echo HOSTNAME:
+hostname
 
 # Gather Disk/Partition, Usage, Mount Points (b)
+echo DISKS
 df
 
 # List of network device name (c,d)
-ip a
+echo "NETWORK INTERFACES:" 
+ip a 
 
 # OS/Kernel Version (e)
+echo "KERNEL INFO:"
 cat /etc/os-release
 uname -r 
 
 # Security mode {SELinux} (f)
+echo "SELINUX:"
 sestatus
 
 # Firewall Config (g)
-sudo iptables -L -n -v
+echo "FIREWALL:"
+sudo iptables-save
 
 # Active Repositories (h)
-sudo dnf repolist all | grep enabled
+echo "ACTIVE REPOSITORIES:"
+sudo dnf repolist all 
 
 echo "RPM Packages: " 
 rpm -qa | wc -l
 # ADD NAME OF ALL PACKAGES TO WORD (j)
 
 # DNS Server (k)
+echo "DNS SERVERS:"
 cat /etc/resolv.conf
 
 # List all active users (l)
+echo "USERS:"
 users
 
 # Date/Time of Installation (m)
-uname -a | cut -d' ' -f6-10
+echo "DATE OF INSTALL:"
+uname -v
+
+# Hardware info
+echo "HARDWARE INFO:"
+#HW
+lshw
+
+#CPU
+echo "CPU INFO:"
+lscpu
+
+#MEM
+echo "MEMORY INFO:"
+free -h
+
+#DEV
+echo "DEVICES:"
+mount
+# Running services (o)
+echo "RUNNING SERVICES:"
+systemctl list-unit-files -t service --no-pager | grep enabled | cut -d' ' -f1
+
+# Non-running services [Installed] (p)
+systemctl list-unit-files -t service --no-pager | grep disabled | cut -d' ' -f1
+
+# EXTRA (q)
