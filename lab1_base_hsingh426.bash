89d5cfa4616423e33ca23d62ea82e72d47ef1a8e
Harmanjit Singh <hsingh426@myseneca.ca>
2020-09-25 22:50:57 -0400

    Baseline results for VM1 [Gateway]

--- lab1_base_hsingh426.bash
+++ lab1_base_hsingh426.bash
@@ -1,69 +1,69 @@
 #!/usr/bin/sh
 
 # Print hostname (a)
-echo "HOSTNAME:\n"
+echo -e "\nHOSTNAME:"
 hostname
 
 # Gather Disk/Partition, Usage, Mount Points (b)
-echo "DISKS:\n"
+echo -e "\nDISKS:"
 df
 
 # List of network device name (c,d)
-echo "NETWORK INTERFACES:\n" 
+echo -e "\nNETWORK INTERFACES:" 
 ip a 
 
 # OS/Kernel Version (e)
-echo "KERNEL INFO:\n"
+echo -e "\nKERNEL INFO:"
 cat /etc/os-release
 uname -r 
 
 # Security mode {SELinux} (f)
-echo "SELINUX:\n"
+echo -e "\nSELINUX:"
 sestatus
 
 # Firewall Config (g)
-echo "FIREWALL:\n"
+echo -e "\nFIREWALL:"
 sudo iptables-save
 
 # Active Repositories (h)
-echo "ACTIVE REPOSITORIES:\n"
+echo -e "\nACTIVE REPOSITORIES:"
 sudo dnf repolist all 
 
-echo "RPM Packages:\n"
+echo -e "\nRPM Packages:"
 rpm -qa | wc -l
 # ADD NAME OF ALL PACKAGES TO WORD (j)
 
 # DNS Server (k)
-echo "DNS SERVERS:\n"
+echo -e "\nDNS SERVERS:"
 cat /etc/resolv.conf
 
 # List all active users (l)
-echo "USERS:\n"
+echo -e "\nUSERS:"
 users
 
 # Date/Time of Installation (m)
-echo "DATE OF INSTALL:\n"
+echo -e "\nDATE OF INSTALL:"
 uname -v
 
 # Hardware info
 
 #CPU
-echo "CPU INFO:\n"
+echo -e "\nCPU INFO:"
 lscpu
 
 #MEM
-echo "MEMORY INFO:\n"
+echo -e "\nMEMORY INFO:"
 free -h
 
 #DEV
-echo "DEVICES:\n"
+echo -e "\nDEVICES:"
 mount
 # Running services (o)
-echo "RUNNING SERVICES:\n"
+echo -e "\nRUNNING SERVICES:"
 systemctl list-unit-files -t service --no-pager | grep enabled | cut -d' ' -f1
 
 # Non-running services [Installed] (p)
-echo "NON-RUNNING SERVICES:\n"
+echo -e "\nNON-RUNNING SERVICES:"
 systemctl list-unit-files -t service --no-pager | grep disabled | cut -d' ' -f1
 
 # EXTRA (q)
