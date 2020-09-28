a45b2a468533faa8e5a688c7bd477a9eba13c619
Harmanjit Singh <hsingh426@myseneca.ca>
2020-09-28 18:29:52 -0400

    Added netmask (CIDR)

--- lab2/add-con.bash
+++ lab2/add-con.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 read -p "What is your host address?: " y 
 nmcli connection add type ethernet ifname ens224 con-name lan
-nmcli connection modify lan ipv4.addresses 192.168.24.$y ipv4.gateway 192.168.24.1
+nmcli connection modify lan ipv4.addresses 192.168.24.$y/29 ipv4.gateway 192.168.24.1
 nmcli connection modify lan ipv4.method manual ipv6.method ignore autoconnect yes
