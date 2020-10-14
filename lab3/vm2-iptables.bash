dc8a6cdc86d063ba623f455bdacb22b8a8bff9ed
Harmanjit Singh <harmanjit_singh@live.com>
2020-10-14 13:14:30 -0400

    Updated vm2 iptables script

--- lab3/vm2-iptables.bash
+++ lab3/vm2-iptables.bash
@@ -20,6 +20,9 @@ iptables -A OUTPUT -o lo -j ACCEPT
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED -j ACCEPT
 
+# LOG SYN PACKETS
+iptables -A INPUT -p tcp --dport 80 --syn -j LOG --log-prefix "[iptables] HTTP SYN: "
+
 # ALLOW SSH (INCOMING)
 iptables -A INPUT -p tcp --dport 22 -m conntrack -s 192.168.24.0/29 --ctstate NEW,ESTABLISHED -j ACCEPT
 iptables -A INPUT -p tcp --dport 22 -m conntrack -s 172.17.24.1 --ctstate NEW,ESTABLISHED -j ACCEPT
@@ -34,6 +37,4 @@ iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
 # Accept HTTP
 iptables -A INPUT -p tcp --dport 80 -s 192.168.24.0/29 -j ACCEPT
 
-# LOG SYN PACKETS
-iptables -A INPUT -p tcp --dport 80 --syn -j LOG --log-prefix "[iptables] HTTP SYN: "
 
