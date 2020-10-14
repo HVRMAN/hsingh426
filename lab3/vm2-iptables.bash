a75d5e2fd5ae4aa87867607e2b380d0da31ccd0d
Harmanjit Singh <harmanjit_singh@live.com>
2020-10-13 23:20:15 -0400

    Added VM2 iptables script

--- lab3/vm2-iptables.bash
+++ lab3/vm2-iptables.bash
@@ -8,14 +8,32 @@ fi
 
 iptables -F 
 
-iptables -P INPUT REJECT
-iptables -P FORWARD REJECT
-iptables -P OUTPUT ACCEPT 
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT ACCEPT
+
+# ALLOW LOOPBACK 
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
+
+# ALLOW ESTABLISHED, CONNECTED
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED -j ACCEPT
+
+# ALLOW SSH (INCOMING)
+iptables -A INPUT -p tcp --dport 22 -m conntrack -s 192.168.24.0/29 --ctstate NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp --dport 22 -m conntrack -s 172.17.24.1 --ctstate NEW,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp --sport 22 -m conntrack --ctstate ESTABLISHED -j ACCEPT
+
+
 
 # Accept ICMP 
+iptables -A INPUT -p icmp --icmp-type echo-request -j ACCEPT
+
 
 # Accept HTTP
+iptables -A INPUT -p tcp --dport 80 -s 192.168.24.0/29 -j ACCEPT
 
 # LOG SYN PACKETS
-
+iptables -A INPUT -p tcp --dport 80 --syn -j LOG --log-prefix "[iptables] HTTP SYN: "
 
