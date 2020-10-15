2fcac7b21329bd74de63719bebf4f0bd450212db
Harmanjit Singh <hsingh426@myseneca.ca>
2020-10-14 20:57:28 -0400

    Updates

--- lab3/gw1-iptables.bash
+++ lab3/gw1-iptables.bash
@@ -28,6 +28,7 @@ iptables -A OUTPUT -p tcp --sport 22 -m conntrack --ctstate ESTABLISHED -j ACCEP
 iptables -A FORWARD -i ens192 -o ens224 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 iptables -A FORWARD -i ens224 -o ens192 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 sudo iptables -A FORWARD -i ens192 -o ens224 -p tcp --syn --dport 80 -m conntrack --ctstate NEW -j ACCEPT
+sudo iptables -A FORWARD -i ens192 -o ens224 -p tcp --dport 22 -m conntrack --ctstate NEW -j ACCEPT
 iptables -A FORWARD -i ens224 -o ens192 -j ACCEPT
 iptables -A FORWARD -i ens192 -o ens224 -j ACCEPT
 
@@ -38,6 +39,8 @@ iptables -t nat -A POSTROUTING -o ens192 -j MASQUERADE
 iptables -t nat -A PREROUTING -i ens192 -p tcp --dport 80 -d 172.17.24.1 -j DNAT --to-destination 192.168.24.2
 iptables -t nat -A PREROUTING -i ens192 -p tcp --dport 80 -d 172.17.24.2 -j DNAT --to-destination 192.168.24.2
 iptables -t nat -A POSTROUTING -o ens224 -p tcp --dport 80 -d 192.168.24.2 -j SNAT --to-source 192.168.24.1
+iptables -t nat -A PREROUTING -i ens192 -p tcp --dport 22 -d 172.17.24.2 -j DNAT --to-destination 192.168.24.2
+iptables -t nat -A POSTROUTING -o ens224 -p tcp --dport 22 -d 192.168.24.2 -j SNAT --to-source 192.168.24.1
 
 iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED -j ACCEPT
 
