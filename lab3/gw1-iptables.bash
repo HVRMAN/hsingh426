bbd7f620441d566b3a25a26d2b85e96be4453365
Harmanjit Singh <hsingh426@myseneca.ca>
2020-10-26 00:26:45 -0400

    Finalized Lab 3 GW Script (Stage 1)

--- lab3/gw1-iptables.bash
+++ lab3/gw1-iptables.bash
@@ -17,8 +17,8 @@ iptables -A INPUT -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
 
 # ICMP RULES
-iptables -A INPUT -p icmp --icmp-type 8 -m state --state NEW,ESTABLISHED,RELATED -j ACCEPT
-iptables -A OUTPUT -p icmp --icmp-type 0 -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -p icmp -j ACCEPT
+iptables -A OUTPUT -p icmp -j ACCEPT
 
 # ALLOW ESTABLISHED
 iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
@@ -37,19 +37,12 @@ iptables -A FORWARD -p tcp --dport 22 --syn -j LOG --log-prefix "[iptables] SSH 
 #iptables -t nat -A POSTROUTING -o ens224 -p tcp --dport 80 -d 192.168.24.2 -j SNAT --to-source 192.168.24.1
 
 # ALLOW FORWARDING
-sudo iptables -A FORWARD -i ens224 -s 192.168.24.0/24 -j ACCEPT
-sudo iptables -A FORWARD -i ens192 -o ens224 -s 172.17.10.1/32 -j ACCEPT
+# sudo iptables -A FORWARD -i ens224 -s 192.168.24.0/24 -j ACCEPT
+# sudo iptables -A FORWARD -i ens192 -o ens224 -s 172.17.10.1/32 -j ACCEPT
 
 
-#iptables -A FORWARD -i ens192 -o ens224 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-#iptables -A FORWARD -i ens224 -o ens192 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
-#sudo iptables -A FORWARD -i ens192 -o ens224 -p tcp --syn --dport 80 -m conntrack --ctstate NEW -j ACCEPT
-#sudo iptables -A FORWARD -i ens192 -o ens224 -p tcp --syn --dport 22 -m conntrack --ctstate NEW -j ACCEPT
-
-
-# SSH FORWARDING RULES
-
-iptables -A OUTPUT -m conntrack --ctstate ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i ens192 -o ens224 -j ACCEPT
+iptables -A FORWARD -i ens224 -o ens192 -j ACCEPT
 
 # MASQUERADE ALL IPS
 iptables -t nat -A POSTROUTING -o ens192 -j MASQUERADE 
