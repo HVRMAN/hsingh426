004a23e220c79a2c0fa7f8382cec272c2b780009
Harmanjit Singh <hsingh426@myseneca.ca>
2020-11-09 15:32:02 -0500

    Update

--- lab4/vm2-iptables.sh
+++ lab4/vm2-iptables.sh
@@ -8,16 +8,17 @@ fi
 
 iptables -F 
 
-iptables -P INPUT DROP
-iptables -P FORWARD DROP
+iptables -P INPUT REJECT
+iptables -P FORWARD REJECT
 iptables -P OUTPUT ACCEPT
 
 iptables -A INPUT -i lo -j ACCEPT
 iptables -A OUTPUT -o lo -j ACCEPT
 
 # ICMP
-iptables -A INPUT -p ICMP --icmp-type echo-request -j ACCEPT
-iptables -A INPUT -p ICMP --icmp-type echo-reply -j ACCEPT
+iptables -A INPUT -p icmp --icmp-type echo-request -s 172.16.0.1 -j ACCEPT
+iptables -A INPUT -p icmp --icmp-type echo-request -s 192.168.24.0/24 -j ACCEPT
+iptables -A INPUT -p icmp --icmp-type echo-reply -j ACCEPT
 
 # DNS
 iptables -A OUTPUT -p udp -d 192.168.24.0/24 --dport 53 \
@@ -32,6 +33,19 @@ iptables -A OUTPUT -p tcp -d 192.168.24.0/24 --dport 53 \
 iptables -A INPUT -p tcp -s 192.168.24.0/24 --sport 53 \
       	-m state --state ESTABLISHED -j ACCEPT
 
+iptables -A OUTPUT -p udp -d 172.16.0.1 --dport 53 \
+	-m state --state NEW,ESTABLISHED -j ACCEPT
+
+iptables -A INPUT -p udp -s 172.16.0.1 --sport 53 \
+	-m state --state ESTABLISHED -j ACCEPT
+
+iptables -A OUTPUT -p tcp -d 172.16.0.1 --dport 53 \
+	-m state --state NEW,ESTABLISHED -j ACCEPT
+
+iptables -A INPUT -p tcp -s 172.16.0.1 --sport 53 \
+      	-m state --state ESTABLISHED -j ACCEPT
+
+
 # SSH
 iptables -A INPUT -p tcp -s 192.168.24.0/24 --dport 22 \
 	-m state --state NEW,ESTABLISHED -j ACCEPT
@@ -43,8 +57,6 @@ iptables -A OUTPUT -p tcp -d 192.168.24.0/24 --sport 22 \
 	-m state --state NEW,ESTABLISHED -j ACCEPT
 
 iptables -A INPUT -p tcp --dport 22 --syn -j LOG --log-prefix "[iptables] EXTERNAL SSH SYN: "
-iptables -A INPUT -p tcp --dport 22 --syn -j REJECT
+iptables -A INPUT -p tcp --dport 22 --syn -j DROP
 iptables -A INPUT -j LOG --log-prefix "[iptables] UNKNOWN SENDER: "
-iptables -A INPUT -s 192.168.24.0/24 -j REJECT
-iptables -A INPUT -j LOG --log-prefix "[iptables] UNEXPECTED TRAFFIC: "
 
